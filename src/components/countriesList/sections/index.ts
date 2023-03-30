1e7a9ca02189cc714c4eee69a752f13d19db5446
ArendtKacper <k.arendt@gridaly.com>
2023-03-30 22:21:53 +0200

    feat add vercel.json

---
+++ src/components/countriesList/sections/index.ts
@@ -0,0 +1,1 @@
+export * from 'src/components/countriesList/sections/CountryListItem';
--- src/components/countriesList/views/CountriesList.tsx
+++ src/components/countriesList/views/CountriesList.tsx
@@ -1,17 +1,19 @@
+import { useTranslation } from 'react-i18next';
 import { useMemo } from 'react';
 import { generatePath, useNavigate, useParams } from 'react-router-dom';
 
 // MATERIAL
 import List from '@mui/material/List';
+
 import ListSubheader from '@mui/material/ListSubheader';
 
 // MODELS
 import { possibleCountries } from 'src/redux/slices/news/models';
+
 import { routes } from 'src/routes/routes';
 
 // COMPONENTS
-import { CountryListItem } from 'src/components/countriesList/sections/CountryListItem';
-import { useTranslation } from 'react-i18next';
+import { CountryListItem } from 'src/components/countriesList/sections';
 
 export const CountriesList = () => {
 	const { id } = useParams();
